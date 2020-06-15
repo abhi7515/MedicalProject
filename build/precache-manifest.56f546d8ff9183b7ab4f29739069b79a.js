b296d6b97f1ddad03089c91771eccefc84a8e107
Abhinav Shukla <abhishukla965@gmail.com>
2020-06-16 03:18:34 +0530

    Add files via upload

---
+++ build/precache-manifest.56f546d8ff9183b7ab4f29739069b79a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "98a68e220f095dd66e33",
+    "url": "/static/css/main.bf4787a3.chunk.css"
+  },
+  {
+    "revision": "98a68e220f095dd66e33",
+    "url": "/static/js/main.98a68e22.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "27451808d74f6574f4ea",
+    "url": "/static/js/2.27451808.chunk.js"
+  },
+  {
+    "revision": "f39bda7d6111d88e912432eae77023c4",
+    "url": "/static/media/avatar.f39bda7d.png"
+  },
+  {
+    "revision": "9f5cd566f23641ef64ee5598210ff4dc",
+    "url": "/static/media/1.9f5cd566.png"
+  },
+  {
+    "revision": "586806cd047e33b5ee7a43fc4252208d",
+    "url": "/static/media/mountains.586806cd.jpg"
+  },
+  {
+    "revision": "20df1fd3e7515768b63c59780a40be1d",
+    "url": "/static/media/logo.20df1fd3.svg"
+  },
+  {
+    "revision": "405dcaaa67d1465faf202de68b96d4a8",
+    "url": "/static/media/integrate.405dcaaa.svg"
+  },
+  {
+    "revision": "4ff84f39bf5abbe374459bffc2449a21",
+    "url": "/static/media/testimonials.4ff84f39.svg"
+  },
+  {
+    "revision": "17571ddde740bf0c6b0641fcf4805e08",
+    "url": "/static/media/doctors.17571ddd.svg"
+  },
+  {
+    "revision": "cd84fcf8e24fcd15592667996666e7f4",
+    "url": "/index.html"
+  }
+];
